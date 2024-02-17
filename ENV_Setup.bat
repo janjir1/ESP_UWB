bc2752a04b5392964b605354ae49ccb219147c40
janjir1 <janjiri.bauer@seznam.cz>
2024-02-17 15:02:09 +0100

    init

---
+++ ENV_Setup.bat
@@ -0,0 +1,15 @@
+set "repo_url=https://github.com/janjir1/build_settitngs.git"
+
+set "destination_dir=%~dp0build_settitngs"
+
+git clone %repo_url% %destination_dir%
+
+set "repo_url=https://github.com/janjir1/components.git"
+
+set "destination_dir=%~dp0components"
+
+git clone %repo_url% %destination_dir%
+
+setx ESP_UWB_ROOT_DIR "%~dp0"
+
+pause
